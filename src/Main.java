f18e0268c6e915bb5ea3a2bcf0a558fed22bc5f7
Aidan Border <planes4u@outlook.com>
2022-02-15 10:39:49 -0500

    :P

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,10 @@ import java.util.Scanner;
 
 public class Main {
 
-	/*  main()
-	 *  Parameters: String[] args
-	 *  Returns: Void
-	 *  Description: Method invoked by the Java runtime when we run the project or run Main.java directly. */
+	/* main()
+	 * Parameters: String[] args
+	 * Returns: Void
+	 * Description: Method invoked by the Java runtime when we run the project or run Main.java directly. */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int numCommands = in.nextInt();
@@ -45,10 +45,10 @@ public class Main {
 		} while (numCommandsExecuted < numCommands);
 	}
 
-	/*  mocsMath()
-	 *  Parameters: Scanner in
-	 *  Returns: Void
-	 *  Description: Wrapper method for recurringMocsMath. This method's job is to start the recursion going and to
+	/* mocsMath()
+	 * Parameters: Scanner in
+	 * Returns: Void
+	 * Description: Wrapper method for recurringMocsMath. This method's job is to start the recursion going and to
 	 * print the result.*/
 	public static void mocsMath(Scanner in) {
 		int userNum = in.nextInt();
@@ -56,10 +56,10 @@ public class Main {
 		System.out.printf("MocsMath:  %d\n\n", recurringMocsMath(userNum));
 	}
 
-	/*  recurringMocsMath()
-	 *  Parameters: int userNum
-	 *  Returns: long
-	 *  Description: Method to compute the sums of the factorial of a given number.
+	/* recurringMocsMath()
+	 * Parameters: int userNum
+	 * Returns: long
+	 * Description: Method to compute the sums of the factorial of a given number.
 	 * Special: Method utilizes recursion.*/
 	public static long recurringMocsMath(int userNum) {
 		if (userNum == 0) {
@@ -70,10 +70,10 @@ public class Main {
 		}
 	}
 
-	/*  factorial()
-	 *  Parameters: long userNum
-	 *  Returns: long
-	 *  Description: Supporting method for recurringMocsMath.
+	/* factorial()
+	 * Parameters: long userNum
+	 * Returns: long
+	 * Description: Supporting method for recurringMocsMath.
 	 * Special: Method utilizes recursion.*/
 	public static long factorial(long userNum) {
 		if (userNum == 0) {
@@ -83,10 +83,10 @@ public class Main {
 		}
 	}
 
-	/*  mocsShape()
-	 *  Parameters: Scanner in
-	 *  Returns: void
-	 *  Description: Wrapper method for mocsShapeForward and mocsShapeBackward. */
+	/* mocsShape()
+	 * Parameters: Scanner in
+	 * Returns: void
+	 * Description: Wrapper method for mocsShapeForward and mocsShapeBackward. */
 	public static void mocsShape(Scanner in) {
 		int lowerBound = in.nextInt();
 		int upperBound = in.nextInt();
@@ -95,6 +95,11 @@ public class Main {
 		mocsShapeBackward(lowerBound, upperBound);
 	}
 
+	/* mocsShapeForward()
+	 * Parameters: int lowerBound, int upperBound
+	 * Returns: void
+	 * Description: This method prints the first half of the mocsShape command
+	 * Special: Method utilized recursion */
 	private static void mocsShapeForward(int lowerBound, int upperBound) {
 		if (lowerBound > upperBound) {
 			return;
@@ -107,6 +112,11 @@ public class Main {
 		mocsShapeForward(lowerBound + 1, upperBound);
 	}
 
+	/* mocsShapeBackward()
+	 * Parameters: int lowerBound, int upperBound
+	 * Returns: void
+	 * Description: This method prints the second half of the mocsShape command
+	 * Special: Method utilized recursion */
 	private static void mocsShapeBackward(int lowerBound, int upperBound) {
 		if (upperBound < lowerBound) {
 			return;
@@ -119,6 +129,10 @@ public class Main {
 		mocsShapeBackward(lowerBound, upperBound - 1);
 	}
 
+	/* mocsGame()
+	 * Parameters: Scanner in
+	 * Returns: Void
+	 * Description: Wrapper method for the recursiveMocsGame method.*/
 	public static void mocsGame(Scanner in) {
 		int startingDollars = in.nextInt();
 
@@ -132,6 +146,11 @@ public class Main {
 		}
 	}
 
+	/* recursiveMocsGame()
+	 * Parameters: int remainingDollars
+	 * Returns: boolean
+	 * Description: Method to run the mocsGame command.
+	 * Special: Method utilizes recursion.*/
 	public static boolean recursiveMocsGame(int remainingDollars) {
 		if (remainingDollars < 42) {
 			return false; // Return false here because we've gone under 42 dollars, which means the player's lost the
@@ -152,9 +171,14 @@ public class Main {
 				return recursiveMocsGame(remainingDollars - dollarsGivenBack);
 			}
 		}
-		return false; // fuck you java. Piece of shit.
+		return false;
 	}
 
+	/* mocsHop()
+	 * Parameters: Scanner in
+	 * Returns: void
+	 * Description: Hopscotch with Mocsie!
+	 * Special: N/A*/
 	public static void mocsHop(Scanner in) {
 		int initPosition = in.nextInt();
 		int size = in.nextInt();
@@ -174,6 +198,11 @@ public class Main {
 		System.out.println();
 	}
 
+	/* recursiveMocHop()
+	 * Parameters: int[] squares, int myIndex, int moveCounter
+	 * Returns: boolean
+	 * Description: Actually computing mocsHop
+	 * Special: Method utilizes recursion.*/
 	public static boolean recursiveMocsHop(int[] squares, int myIndex, int moveCounter) {
 		if ( moveCounter > squares.length || myIndex < 0 || myIndex >= squares.length) {
 			return false;
